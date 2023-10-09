1b0f6a0410c734218260d1eb1c30515e37c5ee0a
Tsog <tsogsummit@gmail.com>
2023-10-09 12:10:30 +0800

    js update
    adding password alert

--- main.js
+++ main.js
@@ -17,4 +17,9 @@ function validateForm() {
         alert("Please enter a valid email address.");
         return;
     }
+
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters long.");
+        return;
+    }
 }
