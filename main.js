6eb5a97d1ae717617cda74c0ed31319a7505b503
Tsog <tsogsummit@gmail.com>
2023-10-09 12:08:32 +0800

    js update
    username alert

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function validateForm() {
     var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    
+    if (username == "") {
+        alert("Username must be filled out");
+        return false;
+    }
 }
