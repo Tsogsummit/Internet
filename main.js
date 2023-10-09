a757d8cdd9875539fbff28bb8d16eb8a519af32e
Tsog <tsogsummit@gmail.com>
2023-10-09 12:31:30 +0800

    js update
    html wrong linked and debugged it

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 function validateForm() {
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+    var username = document.getElementById("username")?.value;
+    var email = document.getElementById("email")?.value;
+    var password = document.getElementById("password")?.value;
     
     if (username == "") {
         alert("Username must be filled out");
         return false;
     }
+    
 
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (email == "") {
         alert("Email must be filled out");
         return false;
     }
+    
     else if (!emailRegex.test(email)) {
         alert("Please enter a valid email address.");
         return;
     }
-
-    if (password.length < 8) {
+    if(password == "") {
+        alert("Password must be filled out");
+        return false;
+    }
+    else if (password.length < 8) {
         alert("Password must be at least 8 characters long.");
         return;
     }
