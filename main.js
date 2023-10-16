aee83fa72c39385036b7fab644a59a46fe1c8bf5
Tsog <tsogsummit@gmail.com>
2023-10-16 10:42:25 +0800

    merging
    merging

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-  const firstNameInput = document.getElementById("firstname");
-  const lastNameInput = document.getElementById("lastname");
-  const ageInput = document.getElementById("age");
-  const sexInput = document.getElementById("sex");
-  const streetInput = document.getElementById("street");
-  const countryInput = document.getElementById("country");
-  const birthDateInput = document.getElementById("birthdate");
-  const submitBtn = document.getElementById("submit-button");
-
-  function checkForm() {
-    if (
-      firstNameInput.value &&
-      lastNameInput.value &&
-      ageInput.value &&
-      sexInput.value &&
-      streetInput.value &&
-      countryInput.value &&
-      birthDateInput.value
-    ) {
-      submitBtn.classList.remove("hidden");
-    } else {
-      submitBtn.classList.add("hidden");
-    }
-  }
-
-  firstNameInput.addEventListener("input", checkForm);
-  lastNameInput.addEventListener("input", checkForm);
-  ageInput.addEventListener("input", checkForm);
-  sexInput.addEventListener("input", checkForm);
-  streetInput.addEventListener("input", checkForm);
-  countryInput.addEventListener("input", checkForm);
-  birthDateInput.addEventListener("input", checkForm);
-
 function validateForm() {
     var username = document.getElementById("username")?.value;
     var email = document.getElementById("email")?.value;
