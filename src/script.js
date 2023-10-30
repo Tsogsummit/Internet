ff5c8ea589d1ffb7edddebb5e2a1e2c0573ff8aa
ariuna221 <63195700+ariuna221@users.noreply.github.com>
2023-10-30 12:17:21 +0800

    cookie
    added cookie

--- src/script.js
+++ src/script.js
@@ -3,6 +3,41 @@ var email;
 var password;
 var phone;
 
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
+
 function validateForm() {
     username = document.getElementById("username")?.value;
     email = document.getElementById("email")?.value;
@@ -97,6 +132,8 @@ function getCountryData(){
 country_sel.addEventListener("change", function() {
     phone_text.value = country_data[country_sel.selectedIndex - 1].dial_code;
 });
+
+
 
 
 var country_data = [
